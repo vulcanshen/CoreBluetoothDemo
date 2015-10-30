64a287749ab958fc795b05e58995a780ead093d9
shenyun <shenyun23.4@gmail.com>
2015-10-30 17:25:28 +0800

    ViewController 1

---
+++ CoreBlueToothDemo/PeripheralCellTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PeripheralCellTableViewCell.swift
+//  CoreBlueToothDemo
+//
+//  Created by kutai on 2015/10/30.
+//  Copyright © 2015年 shenyun. All rights reserved.
+//
+
+import UIKit
+
+class PeripheralCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbUUID: UILabel!
+    @IBOutlet weak var lbRSSI: UILabel!
+    @IBOutlet weak var lbConntable: UILabel!
+    @IBOutlet weak var lbName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CoreBlueToothDemo/ViewController1.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  CoreBlueToothDemo
+//
+//  Created by kutai on 2015/10/30.
+//  Copyright © 2015年 shenyun. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+class ViewController1: UITableViewController, CBCentralManagerDelegate {
+
+    
+    var btCentralManager: CBCentralManager!
+    var btPeripherals: [CBPeripheral] = []
+    var btRSSIs: [NSNumber] = []
+    var btConnectable: [Int] = []
+    @IBOutlet weak var bbRefresh: UIBarButtonItem!
+    
+    
+    @IBAction func actionRefresh(sender: UIBarButtonItem!) {
+        bbRefresh.enabled = false
+        btConnectable.removeAll()
+        btPeripherals.removeAll()
+        btRSSIs.removeAll()
+        tableView.registerNib(UINib(nibName: "PeripheralCellTableViewCell", bundle: nil), forCellReuseIdentifier: "PeripheralCellTableViewCell")
+        
+        NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: "stopScan", userInfo: nil, repeats: false)
+        btCentralManager.scanForPeripheralsWithServices(nil, options: nil)
+    }
+    
+    func stopScan() {
+        btCentralManager.stopScan()
+        bbRefresh.enabled = true
+        tableView.reloadData()
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: PeripheralCellTableViewCell = tableView.dequeueReusableCellWithIdentifier("PeripheralCellTableViewCell") as! PeripheralCellTableViewCell
+        cell.lbConntable.text = btConnectable[indexPath.row].description
+        cell.lbName.text = btPeripherals[indexPath.row].name
+        cell.lbRSSI.text = btRSSIs[indexPath.row].description
+        cell.lbUUID.text = btPeripherals[indexPath.row].identifier.UUIDString
+        cell.accessoryType = .DisclosureIndicator
+        return cell
+
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 130.5
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return btPeripherals.count
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        btCentralManager = CBCentralManager(delegate: self, queue: nil)
+
+    }
+    
+
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        print(central.state)
+    }
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        let temp = btPeripherals.filter { (pl) -> Bool in
+            return pl.identifier.UUIDString == peripheral.identifier.UUIDString
+        }
+        
+        if temp.count == 0 {
+            btPeripherals.append(peripheral)
+            btRSSIs.append(RSSI)
+            btConnectable.append(Int(advertisementData[CBAdvertisementDataIsConnectable]!.description)!)
+        }
+    }
+
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
